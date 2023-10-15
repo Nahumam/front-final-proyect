a859d1805b183ebaa0c1f46a637adf9c884a1f7e
MatiasRodrig <mayutefracran@gmail.com>
2023-10-14 23:27:15 -0300

    v0.2

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,17 +12,19 @@ function Navbar() {
                         <img src={Logo} alt="" />
                     </a>
                 </div>
-                <ul className='navbar-list'>
-                    <NavItem title="Compre Ahora" dropdownItems={['Opción 1.1', 'Opción 1.2', 'Opción 1.3']} />
-                    <NavItem title="Muebles Para El Hogar" dropdownItems={['Opción 2.1', 'Opción 2.2', 'Opción 2.3']} />
-                    <NavItem title="Sala De Estar" dropdownItems={['Opción 3.1', 'Opción 3.2', 'Opción 3.3']} />
-                    <NavItem title="Sobre Nosotros" dropdownItems={['Opción 4.1', 'Opción 4.2', 'Opción 4.3']} />
-                    <NavItem title="Contactanos" dropdownItems={['Opción 5.1', 'Opción 5.2', 'Opción 5.3']} />
-                </ul>
-                <ul className='login-register'>
-                    <li className='login'><button>Iniciar Sesión</button></li>
-                    <li className='register'><button>Registrarse</button></li>
-                </ul>
+                <div className="right-container-navbar">
+                    <ul className='navbar-list'>
+                        <NavItem title="Compre Ahora" dropdownItems={['Opción 1.1', 'Opción 1.2', 'Opción 1.3']} className='first-button-navbar' />
+                        <NavItem title="Muebles Para El Hogar" dropdownItems={['Opción 2.1', 'Opción 2.2', 'Opción 2.3']} />
+                        <NavItem title="Sala De Estar" dropdownItems={['Opción 3.1', 'Opción 3.2', 'Opción 3.3']} />
+                        <NavItem title="Sobre Nosotros" dropdownItems={['Opción 4.1', 'Opción 4.2', 'Opción 4.3']} />
+                        <NavItem title="Contactanos" dropdownItems={['Opción 5.1', 'Opción 5.2', 'Opción 5.3']} />
+                    </ul>
+                    <ul className='login-register'>
+                        <li className='login'><a href=""><button>Iniciar Sesión</button></a></li>
+                        <li className='register'><a href=""><button>Registrarse</button></a></li>
+                    </ul>
+                </div>
             </section>
         </>
     );
@@ -38,10 +40,12 @@ function NavItem({ title, dropdownItems }) {
     const toggleLeaveDropdown = () => {
         setShowDropdown(false)
     }
+    
 
     return (
         <li>
             <button onClick={toggleDropdown} onMouseLeave={toggleLeaveDropdown} className='bttn'>
+                <div>
                 {title}
                 {showDropdown && (
                     <ul className="dropdown">
@@ -49,9 +53,9 @@ function NavItem({ title, dropdownItems }) {
                             <li key={index} className='bttn-open'><a href="">{item}</a></li>
                         ))}
                     </ul>
-                )}
-                
-                <FiChevronDown className='arrow-down'/>
+                    )}</div>
+
+                <FiChevronDown className='arrow-down' />
             </button>
         </li>
     );

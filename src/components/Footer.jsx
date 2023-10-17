8a78c8330b315fea58b8a0cac03f48a564e63be9
MatiasRodrig <mayutefracran@gmail.com>
2023-10-17 20:48:09 -0300

    v0.4

---
+++ src/components/Footer.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function Footer() {
+    return (
+        <>
+            <section className="footer">
+                <div className="suscribe">
+                    <div className="logo-footer">
+                        <img src="" alt="" />
+                    </div>
+                    <p className='join'></p>
+                    <input type="email" name="enter-email" id="enter-email" placeholder='Ingresa tu Email' />
+                    <button type="submit" value="SUSCRIBETE">Suscribete</button>
+                    <p className="privacy-policy"></p>
+                </div>
+                <div className="about-us"></div>
+                <div className="follow-us"></div>
+            </section>
+        </>
+    )
+}
+
+export default Footer
---
+++ src/components/StylishCollection.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function StylishCollection() {
+    return (
+        <>
+            <section className="collection">
+                <h1 className="collection-title">
+                    Elegante Colección de Muebles
+                </h1>
+                <p>Mantente informado con nuestras publicaciones en el blog sobre muebles modernos y de moda en la industria</p>
+                <div className="collection-buttons">
+                </div>
+                <div className="collection-grid"></div>
+            </section>
+        </>
+    )
+}
+
+export default StylishCollection

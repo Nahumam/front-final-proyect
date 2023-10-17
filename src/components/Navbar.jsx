7998ad09f6d49d7c8b9890f9693651dc11644a79
MatiasRodrig <mayutefracran@gmail.com>
2023-10-17 11:41:26 -0300

    v0.3

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,11 +14,14 @@ function Navbar() {
                 </div>
                 <div className="right-container-navbar">
                     <ul className='navbar-list'>
-                        <NavItem title="Compre Ahora" dropdownItems={['Opción 1.1', 'Opción 1.2', 'Opción 1.3']} className='first-button-navbar' />
-                        <NavItem title="Muebles Para El Hogar" dropdownItems={['Opción 2.1', 'Opción 2.2', 'Opción 2.3']} />
-                        <NavItem title="Sala De Estar" dropdownItems={['Opción 3.1', 'Opción 3.2', 'Opción 3.3']} />
-                        <NavItem title="Sobre Nosotros" dropdownItems={['Opción 4.1', 'Opción 4.2', 'Opción 4.3']} />
-                        <NavItem title="Contactanos" dropdownItems={['Opción 5.1', 'Opción 5.2', 'Opción 5.3']} />
+                        <div
+                            className='first-button-navbar'>
+                            <FirstNavItem title="Compre Ahora" dropdownItems={['Opción 1.1', 'Opción 1.2', 'Opción 1.3']} dropdownLinks={['https://wordwall.net/resource/36271235/english/first-conditional-what-will-happen-if', '/link2', '/link3']} />
+                        </div>
+                        <NavItem title="Muebles Para El Hogar" dropdownItems={['Opción 2.1', 'Opción 2.2', 'Opción 2.3']} dropdownLinks={['/link4', '/link5', '/link6']} />
+                        <NavItem title="Sala De Estar" dropdownItems={['Opción 3.1', 'Opción 3.2', 'Opción 3.3']} dropdownLinks={['/link7', '/link8', '/link9']} />
+                        <NavItem title="Sobre Nosotros" dropdownItems={['Opción 4.1', 'Opción 4.2', 'Opción 4.3']} dropdownLinks={['/link10', '/link11', '/link12']} />
+                        <NavItem title="Contactanos" dropdownItems={['Opción 5.1', 'Opción 5.2', 'Opción 5.3']} dropdownLinks={['/link13', '/link14', '/link15']} />
                     </ul>
                     <ul className='login-register'>
                         <li className='login'><a href=""><button>Iniciar Sesión</button></a></li>
@@ -30,7 +33,39 @@ function Navbar() {
     );
 }
 
-function NavItem({ title, dropdownItems }) {
+
+function FirstNavItem({ title: firstTitle, dropdownItems: firstDropdown, dropdownLinks: firstDropdownLinks }) {
+    const [showFirstDropdown, setShowFirstDropdown] = useState(false);
+
+    const toggleFirstDropdown = () => {
+        setShowFirstDropdown(!showFirstDropdown);
+    };
+
+    const toggleLeaveFirstDropdown = () => {
+        setShowFirstDropdown(false)
+    }
+
+    return (
+        <li>
+            <button onClick={toggleFirstDropdown} onMouseLeave={toggleLeaveFirstDropdown} className='first-bttn'>
+                <div>
+                    {firstTitle}
+                    {showFirstDropdown && (
+                        <ul className="dropdown">
+                            {firstDropdown.map((item, index) => (
+                                <li key={index} className='first-bttn-open'><a href={firstDropdownLinks[index]}>{item}</a></li>
+                            ))}
+                        </ul>
+                    )}</div>
+
+                <FiChevronDown className='first-arrow-down' />
+            </button>
+        </li>
+    );
+}
+
+
+function NavItem({ title, dropdownItems, dropdownLinks }) {
     const [showDropdown, setShowDropdown] = useState(false);
 
     const toggleDropdown = () => {
@@ -40,19 +75,18 @@ function NavItem({ title, dropdownItems }) {
     const toggleLeaveDropdown = () => {
         setShowDropdown(false)
     }
-    
 
     return (
         <li>
             <button onClick={toggleDropdown} onMouseLeave={toggleLeaveDropdown} className='bttn'>
                 <div>
-                {title}
-                {showDropdown && (
-                    <ul className="dropdown">
-                        {dropdownItems.map((item, index) => (
-                            <li key={index} className='bttn-open'><a href="">{item}</a></li>
-                        ))}
-                    </ul>
+                    {title}
+                    {showDropdown && (
+                        <ul className="dropdown">
+                            {dropdownItems.map((item, index) => (
+                                <li key={index} className='bttn-open'><a href={dropdownLinks[index]}>{item}</a></li>
+                            ))}
+                        </ul>
                     )}</div>
 
                 <FiChevronDown className='arrow-down' />
@@ -60,5 +94,5 @@ function NavItem({ title, dropdownItems }) {
         </li>
     );
 }
-
-export default Navbar;
+ 
+export default Navbar

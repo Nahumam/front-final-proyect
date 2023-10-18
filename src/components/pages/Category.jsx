47fd398586de84fe296fdd787c7cca9b6d7db0b8
MatiasRodrig <mayutefracran@gmail.com>
2023-10-17 22:15:03 -0300

    v0.4.1

---
+++ src/components/pages/Category.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+function Living() {
+  return (
+    <>
+      <section className="living-room">
+        <h1 className="living-title">Sala de Estar</h1>
+        <div className="listof-products">
+          <h2><span className="home">Inicio</span>Lista de Productos</h2>
+          <div className="view">
+            <h3>Vista</h3>
+            <img src="" alt="" />
+            <img src="" alt="" />
+          </div>
+        </div>
+
+        <div className="category-buttons">
+          <a href="" className="all-furniture-button"><button></button></a>
+          <a href="" className="bedroom-button"><button></button></a>
+          <a href="" className="living-room-button"><button></button></a>
+          <a href="" className="home-office-button"><button></button></a>
+          <a href="" className="more-button"><button></button></a>
+      </div>
+      </section>
+    </>
+  )
+}
+
+export default Living
---
+++ src/components/pages/ProductDetail.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function ProductDetail() {
+    return (
+        <>
+            <section className="product-detail">
+                <h2><span className="home">Inicio / Lista de Productos </span>/ <Product/> </h2>
+            </section>
+        </>
+    )
+}
+
+
+
+export default ProductDetail

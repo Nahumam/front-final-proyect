dee8aa9a0f575fbc24c34aa36b3d9b12d2b72c45
MatiasRodrig <mayutefracran@gmail.com>
2023-10-24 20:07:07 -0300

    v0.8

--- src/components/pages/ProductDetail.jsx
+++ src/components/pages/ProductDetail.jsx
@@ -1,11 +1,33 @@
-import React from 'react'
+import { useState } from 'react'
 
 function ProductDetail() {
     return (
         <>
             <section className="product-detail">
-                <h2><span className="home">Inicio / Lista de Productos </span>/ <Product/> </h2>
+                <h2><span className="home">Inicio / Lista de Productos </span>/ <Product /> </h2>
             </section>
+        </>
+    )
+}
+
+
+function Product() {
+    const [productos, setProductos] = useState([])
+
+    useEffect(() => {
+        fetch('https://scratchya.com.ar/react/datos.php')
+            .then((response) => {
+                return response.json()
+            })
+            .then((productos) => {
+                setProductos(productos)
+            })
+    }, [])
+
+
+    return (
+        <>
+
         </>
     )
 }

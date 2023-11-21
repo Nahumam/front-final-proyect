dd0b7b89f2766547badf0dcf3498fd0ef21f64d0
MatiasRodrig <mayutefracran@gmail.com>
2023-11-20 23:32:55 -0300

    v2.3

--- src/components/StylishCollection.jsx
+++ src/components/StylishCollection.jsx
@@ -1,7 +1,17 @@
 import '../styles/stylishcollection.css'
 import { Link } from 'react-router-dom'
+import { useState, useEffect } from 'react';
 
 function StylishCollection() {
+    const [productos, setProductos] = useState([]);
+
+    useEffect(() => {
+        fetch('https://e-commerce-api-bwhg.onrender.com/api/product')
+            .then(response => response.json())
+            .then(data => setProductos(data.slice(0, 8)))
+            .catch(error => console.error('error', error))
+    }, []);
+
     return (
         <>
             <section className="collection">
@@ -10,26 +20,29 @@ function StylishCollection() {
                 </h1>
                 <p>Mantente informado con nuestras publicaciones en el blog sobre muebles modernos y de moda en la industria</p>
                 <div className="collection-buttons">
-                    <button> <Link> Todos los Muebles </Link></button> 
-                    <button><Link>Dormitorio</Link> </button>
-                    <button> <Link>Sala de Estar</Link></button>
-                    <button><Link>Oficina en Casa</Link></button>
-                    <button><Link>Comedor</Link></button>
-                    <button><Link>Más</Link></button>
+                    <button><Link to="/">Todos los Muebles</Link></button>
+                    <button><Link to="/dormitorio">Dormitorio</Link></button>
+                    <button><Link to="/sala-de-estar">Sala de Estar</Link></button>
+                    <button><Link to="/oficina-en-casa">Oficina en Casa</Link></button>
+                    <button><Link to="/comedor">Comedor</Link></button>
+                    <button><Link to="/mas">Más</Link></button>
                 </div>
                 <div className="collection-grid">
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
+                    {productos.map(producto => (
+                        <div key={producto.id} className="product-item">
+                            <button>
+                                <Link to={`/detalle/${producto.id}`}>
+                                    <img src={producto.image} alt={producto.name} />
+                                    <h3>{producto.name}</h3>
+                                    <p>${producto.price}</p>
+                                </Link>
+                            </button>
+                        </div>
+                    ))}
                 </div>
             </section>
         </>
     )
 }
 
-export default StylishCollection
+export default StylishCollection;
